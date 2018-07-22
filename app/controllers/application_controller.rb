cb6eed67d42bc786103a0ba78497738a46640844
Adam Kalt <adam.kalt.1@gmail.com>
2018-07-22 19:29:05 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,16 +11,19 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
+    binding.pry
     @user = User.find_by(:username => params[:username])
       if @user != nil && @user.password == params[:password]
         session[:user_id] = @user.id
         redirect to '/account'
-      end
+        else
       erb :error
     end
+  end
 
 
       get '/account' do
+        binding.pry
         @current_user = User.find_by_id(session[:user_id])
         if @current_user
           erb :account
@@ -28,6 +31,8 @@ class ApplicationController < Sinatra::Base
           erb :error
         end
       end
+
+
 
   get '/logout' do
    session.clear
